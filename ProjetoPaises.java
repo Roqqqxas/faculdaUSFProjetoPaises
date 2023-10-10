12af4443f30a109166184bb8a57f421edf32ea8d
Roqqqxas <146894124+Roqqqxas@users.noreply.github.com>
2023-10-10 20:05:31 -0300

    Add files via upload

---
+++ ProjetoPaises.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetopaises;
+
+/**
+ *
+ * @author sp303lab
+ */
+public class ProjetoPaises {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
