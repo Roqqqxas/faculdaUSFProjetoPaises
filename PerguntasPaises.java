a178985e4b972574a27b2807901daaa9c5e28539
Roqqqxas <146894124+Roqqqxas@users.noreply.github.com>
2023-10-10 20:08:05 -0300

    Update PerguntasPaises.java

--- PerguntasPaises.java
+++ PerguntasPaises.java
@@ -4,10 +4,46 @@
  */
 package com.mycompany.projetopaises;
 
+
 /**
  *
  * @author sp303lab
  */
 public class PerguntasPaises {
-    
+
+    private final String pergunta;
+    private String opcaoA;
+    private String opcaoB;
+    private String opcaoC;
+    private char respostaCerta;
+
+    public PerguntasPaises(String pergunta, String opcaoA, String opcaoB, String opcaoC, char respostaCerta) {
+
+        this.pergunta = pergunta;
+        this.opcaoA = opcaoA;
+        this.opcaoB = opcaoB;
+        this.opcaoC = opcaoC;
+        this.respostaCerta = respostaCerta;
+
+    }
+
+    public String Pergunta() {
+        return pergunta;
+
+    }
+
+    public void exibirPergunta() {
+        System.out.println(pergunta);
+        System.out.println("a) " + opcaoA);
+        System.out.println("b) " + opcaoB);
+        System.out.println("c) " + opcaoC);
+    }
+
+    public char getRepostaCerta() {
+        return respostaCerta;
+    }
+
+    public boolean verificarespostaCerta(char resposta) {
+        return resposta == respostaCerta;
+    }
 }
